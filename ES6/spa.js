ef725878d0861d6440b7c9e11afa214eafad2029
Mitchell Morgan <mitchemorgan@gmail.com>
2025-08-07 09:08:54 -0600

    React App/imp

---
+++ ES6/spa.js
@@ -0,0 +1,22 @@
+// Single page application using react 
+
+// uses one container to render entire application 
+// page routes will be managed by React router no requests will be made to the server
+// we will have only one index.html file which will be served by the server
+
+//webpack will bundle all the react components and the react router
+//webpack will be used to create loaders (other library properties) and plugins (webpack plugins) to optimize the bundle size
+// webpack will also cretae a develpoment server the application
+//webpack will also create a production build of the application
+//webpack will also create a production build of the application
+//wp will also create source map
+//wp will also create a hot module replacement for application
+
+//bable will be used to transpile the code form Es6 to es5 and react code to browser compatible dode 
+//plugins for minification, uglification, and optimization
+
+//npm init -y
+
+ //npm i webpack webpack-cli webpack-dev-server
+
+ //
